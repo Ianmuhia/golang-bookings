588f4ef6e44608e89666d2dbdbbad877874983e9
ianmuhia <ianmuhia3@gmail.com>
2021-10-17 16:36:56 +0300

    added testing for forms package Form_MinLength function all passing

--- internals/forms/forms.go
+++ internals/forms/forms.go
@@ -24,7 +24,7 @@ func New(data url.Values) *Form {
 
 // Has checks if form field in post is  not empty
 func (f *Form) Has(field string, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 	if x == "" {
 		f.Errors.Add(field, "This field cannot be blank")
 		return false
@@ -48,7 +48,7 @@ func (f *Form) Required(fields ...string) {
 
 // MinLength checks for string minimum length
 func (f *Form) MinLength(field string, length int, r *http.Request) bool {
-	x := r.Form.Get(field)
+	x := f.Get(field)
 	if len(x) < length {
 		f.Errors.Add(field, fmt.Sprintf("This field must be at least %d characters long", length))
 		return false
--- internals/forms/forms_test.go
+++ internals/forms/forms_test.go
@@ -40,3 +40,50 @@ func TestForm_Required(t *testing.T) {
 		t.Error("shows does not have required fields when it does")
 	}
 }
+
+func TestForm_Has(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	has := form.Has("whatever", r)
+	if has {
+		t.Error("form shows has field when it does not")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("a", "a")
+
+	form = New(postedData)
+	has = form.Has("a", r)
+	if !has {
+		t.Error("Shows form does not have field when it does")
+	}
+}
+
+func TestForm_MinLength(t *testing.T) {
+	r := httptest.NewRequest("POST", "/whatever", nil)
+	form := New(r.PostForm)
+
+	form.MinLength("x", 10, r)
+	if form.Valid() {
+		t.Error("form shows minlength for non-existent field")
+	}
+
+	postedData := url.Values{}
+	postedData.Add("some_field", "some value")
+	form = New(postedData)
+
+	form.MinLength("some_field", 100, r)
+	if form.Valid() {
+		t.Error("shows min length if 100 met when data is shorter")
+	}
+
+	postedData = url.Values{}
+	postedData.Add("another_field", "abc123")
+	form = New(postedData)
+
+	form.MinLength("another_field", 1, r)
+	if !form.Valid() {
+		t.Error("shows min length if 1 is not met when data is shorter")
+	}
+}
