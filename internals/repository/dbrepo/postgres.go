38ec056325e48c5590b153fc8e6874266b72a849
ianmuhia <ianmuhia3@gmail.com>
2021-10-23 18:30:13 +0300

    InsertReservation function toinsert reservation into the models

--- internals/repository/dbrepo/postgres.go
+++ internals/repository/dbrepo/postgres.go
@@ -1,5 +1,37 @@
 package dbrepo
 
+import (
+	"context"
+	"github.com/ianmuhia/bookings/internals/models"
+	"time"
+)
+
 func (m *postgresDBRepo) AllUsers() bool {
 	return true
+}
+
+// InsertReservation inserts the reservation into the database
+func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	stmt := ` insert into reservations (
+	first_name, last_name, email, phone, start_date, end_date,
+	room_id, created_at, updated_at) values ( $1,$2,$2,$4,$5,$6,$7,$8,$9)
+	`
+	_, err := m.DB.ExecContext(ctx, stmt,
+		res.FirstName,
+		res.LastName,
+		res.Email,
+		res.Phone,
+		res.StartDate,
+		res.EndDate,
+		res.RoomID,
+		time.Now(),
+		time.Now(),
+	)
+
+	if err != nil {
+		return err
+	}
+	return nil
 }
--- internals/repository/repository.go
+++ internals/repository/repository.go
@@ -1,5 +1,8 @@
 package repository
 
+import "github.com/ianmuhia/bookings/internals/models"
+
 type DatabaseRepo interface {
 	AllUsers() bool
+	InsertReservation(res models.Reservation) error
 }
