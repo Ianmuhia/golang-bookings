26d679bdb3de7ab3e66527538be3e7cdc85f2a0b
ianmuhia <ianmuhia3@gmail.com>
2021-10-23 23:23:44 +0300

    add reservation to db through templates

--- internals/handlers/handlers.go
+++ internals/handlers/handlers.go
@@ -8,6 +8,8 @@ import (
 	"github.com/ianmuhia/bookings/internals/repository"
 	"github.com/ianmuhia/bookings/internals/repository/dbrepo"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/ianmuhia/bookings/internals/config"
 	"github.com/ianmuhia/bookings/internals/forms"
@@ -72,16 +74,43 @@ func (m *Repository) Reservation(w http.ResponseWriter, r *http.Request) {
 //PostReservation handles the posting of a reservation form
 func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
+
 	if err != nil {
 		helpers.ServerError(w, err)
 		return
 	}
 
+	sd := r.Form.Get("start_date")
+	ed := r.Form.Get("end_date")
+
+	//2020-10-10
+	layout := "2006-01-02"
+	startDate, err := time.Parse(layout, sd)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+	endDate, err := time.Parse(layout, ed)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
+	roomID, err := strconv.Atoi(r.Form.Get("room_id"))
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	reservation := models.Reservation{
+		ID:        0,
 		FirstName: r.Form.Get("first_name"),
 		LastName:  r.Form.Get("last_name"),
 		Email:     r.Form.Get("email"),
 		Phone:     r.Form.Get("phone"),
+		StartDate: startDate,
+		EndDate:   endDate,
+		RoomID:    roomID,
+		CreatedAt: time.Time{},
+		UpdatedAt: time.Time{},
+		Room:      models.Room{},
 	}
 	form := forms.New(r.PostForm)
 
@@ -102,6 +131,12 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		)
 		return
 	}
+
+	err = m.DB.InsertReservation(reservation)
+	if err != nil {
+		helpers.ServerError(w, err)
+	}
+
 	m.App.Session.Put(r.Context(), "reservation", reservation)
 	http.Redirect(w, r, "/reservation-summary", http.StatusSeeOther)
 }
--- internals/repository/dbrepo/postgres.go
+++ internals/repository/dbrepo/postgres.go
@@ -14,9 +14,10 @@ func (m *postgresDBRepo) AllUsers() bool {
 func (m *postgresDBRepo) InsertReservation(res models.Reservation) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
+
 	stmt := ` insert into reservations (
 	first_name, last_name, email, phone, start_date, end_date,
-	room_id, created_at, updated_at) values ( $1,$2,$2,$4,$5,$6,$7,$8,$9)
+	room_id, created_at, updated_at) values ( $1,$2,$3,$4,$5,$6,$7,$8,$9) returning id
 	`
 	_, err := m.DB.ExecContext(ctx, stmt,
 		res.FirstName,
